0a19c9614d3f117f0cceb63f53692d8231fc3cd5
Paul Free <paul@paulfree.com>
2014-09-18 08:47:31 -0400

    Initial Check-in

---
+++ function.js
@@ -0,0 +1,68 @@
+this[(typeof lambdaalias != "undefined") ? lambdaalias : "lambda"] = function(l)
+{
+  function _parse(ll)        
+  {                 
+    // separate parameters from body
+    var matches = /(?:\(([^\)]*)\)|([a-z_]\w*))\s*=>(.*)/i.exec(ll);
+  
+    if (!matches) return null;
+    // drop unneeded full match
+    if ( matches.length > 0 ) matches.shift();
+  
+    var p = [];
+    // if matched multiple parameter/parenthesized style
+    if (typeof matches[0] != "undefined")
+    {
+      // extract parameters
+      var re = /[a-z_]\w*/ig;
+      var match;
+  
+      while (match = re.exec(matches[0]))
+        // save parameter  
+        p.push(match[0]);
+    }
+    // if matched non-parenthesized style
+    else if (typeof matches[1] != "undefined" && matches[1] != "") 
+      // save single parameter  
+      p.push(matches[1]);
+    else // no match - syntax error in lambda
+      return null;
+  
+    var b = matches.pop(); // extract the body text
+ 
+    // is it another lambda?
+    var inner = _parse(b);
+    
+    if (inner != null && inner.length > 0)
+    {
+      var b2 = inner.pop();
+      b = "function(" + inner.join(",") + "){" + b2 +  "}";
+    }
+    // if simple expression and not a code block, 
+    // prepend a return if not already there.
+    if (! /^\s*\{(.*)\}\s*$/.test(b) && ! /^\s*return\s+/.test( b ))
+      b = "return " + b ;    
+  
+    // save body
+    p.push(b);
+    
+    return p; 
+  }
+
+  var f = _parse(l);
+  
+  if (f == null || f.length < 1) return null;
+  
+  // try to create a javascript function from parameters and block
+  try
+  {
+    return Function.apply( null, f );
+  }
+  catch(e)
+  {
+    return null ;
+  }
+};
+
+
+
